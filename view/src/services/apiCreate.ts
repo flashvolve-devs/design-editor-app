f23bd5b2cdaf108aab11a43f577c7b857a84746a
JoaoPedroLage <joaopedro.lage@outlook.com>
2022-09-14 10:11:24 -0300

    feat: change endpoints for deployed backend

--- view/src/services/apiCreate.ts
+++ view/src/services/apiCreate.ts
@@ -1,7 +1,8 @@
 import CryptoJS from 'crypto-js';
 
 export default async function create(data: any, path: any) {
-  const URL = `http://localhost:3001/${path}`;
+  // const URL = `http://localhost:3001/${path}`;
+  const URL = `https://design-editor-app-z22dtvdr6q-uc.a.run.app/${path}`;
 
   const md5Password = CryptoJS.MD5(data.password).toString();
 
